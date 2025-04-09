fcb1ca5dc6cd7343534e4d1e4a93f747b4e7f45a
dakshSharma289 <dakshsharma784@gmail.com>
2025-04-09 11:30:54 +0530

    initialized and added boiler plate

---
+++ app.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Wellcome to my Overkill Calculator App" << endl << "You can enter your problem bellow:" << endl;
+    
+    
+    return 0;
+}
