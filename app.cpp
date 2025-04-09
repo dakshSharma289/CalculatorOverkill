ec8ec4ecdd78b50d106c72ce7dcd50c04ffa1969
dakshSharma289 <dakshsharma784@gmail.com>
2025-04-09 12:20:16 +0530

    added an infinite loop and a loop to traverse the input and find where and what type of brackets are present

--- app.cpp
+++ app.cpp
@@ -1,9 +1,32 @@
 #include<iostream>
+#include<string>
+#include<vector>
+
 using namespace std;
 
 int main(){
-    cout << "Wellcome to my Overkill Calculator App" << endl << "You can enter your problem bellow:" << endl;
-    
-    
+    cout << "Wellcome to my Overkill Calculator App" << endl << "To exit the program, type 'q' in a new line and press enter" << endl << "You can enter your problem bellow:" << endl;
+    while(0 < 1){
+        string input;
+        getline(cin, input);
+
+        if((input == "q") || (input == "Q")){
+            break;
+        }
+//here I'm iterating through the input to figure out where and how many brackets are pestent.
+        vector <int> bracketIndices;
+        vector <bool> bracketType;
+        for(int i = 0; i < input.length(); i++){
+            if(input[i] == '('){
+                bracketIndices.push_back(i);
+                bracketType.push_back(true);
+            }else if(input[i] == ')'){
+                bracketIndices.push_back(i);
+                bracketType.push_back(false);
+            }
+        }
+    }
+    cout<<endl<< "program exited successfully.";
+
     return 0;
 }
