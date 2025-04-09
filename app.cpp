dad9ce46ba8b1968e76ac0d2a4479a3d7063adc6
dakshSharma289 <dakshsharma784@gmail.com>
2025-04-09 15:27:01 +0530

    I am right know figuring out how to get around having multiple operators inside a single bracket - part one

--- app.cpp
+++ app.cpp
@@ -25,6 +25,53 @@ int main(){
                 bracketType.push_back(false);
             }
         }
+
+        if(!(bracketIndices.size()%2)){
+            cout<<"you missed a pathentasis \n the police are on thier way!"<<endl;
+        }
+
+        for(int i = (bracketIndices.size()/2); i >= 0; i--){
+            string currentExpression = input.substr(bracketIndices[i], (bracketIndices[bracketIndices.size() - i]) - bracketIndices[i]);
+            string operators;
+            vector <int> operatorPosition;
+            for(int j = 0; j < currentExpression.length(); j++){
+                if (currentExpression[j == '+']){
+
+                    operators.append("+");
+                    operatorPosition.push_back(j);
+
+                }else if (currentExpression[j == '-']){
+
+                    operators.append("-");
+                    operatorPosition.push_back(j);
+                    
+                }else if (currentExpression[j == '*']){
+
+                    operators.append("*");
+                    operatorPosition.push_back(j);
+                    
+                }else if (currentExpression[j == '/']){
+
+                    operators.append("/");
+                    operatorPosition.push_back(j);
+                    
+                } 
+            }
+            for(int j = 0; j < currentExpression.length(); j++){
+                for(int t = 0; t < operators.length(); t++){
+                    if(operators[t] == '/'){
+                        int k = 0;
+                        int counter = 0;
+                        while(!k){
+                            if(currentExpression[operatorPosition[t] - counter] == ' '){
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
     cout<<endl<< "program exited successfully.";
 
